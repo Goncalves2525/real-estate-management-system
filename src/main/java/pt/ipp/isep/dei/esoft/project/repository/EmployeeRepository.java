582d48e51b4c0685d2bac928532ddb079b26ddfa
Luis Leal <1100253@isep.ipp.pt>
2023-04-28 22:15:35 +0100

    Employee repositories FIX

--- src/main/java/pt/ipp/isep/dei/esoft/project/repository/EmployeeRepository.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/repository/EmployeeRepository.java
@@ -1,6 +1,5 @@
 package pt.ipp.isep.dei.esoft.project.repository;
 
-import pt.ipp.isep.dei.esoft.project.domain.Agent;
 import pt.ipp.isep.dei.esoft.project.domain.Employee;
 
 import java.util.ArrayList;
@@ -9,33 +8,7 @@ import java.util.Optional;
 
 public class EmployeeRepository {
 
-    private final List<Agent> AgentList = new ArrayList<>();
     private final ArrayList<Employee> employeeList = new ArrayList<>();
-
-    public Optional<Agent> add(Agent agent) {
-
-        Optional<Agent> newAgent = Optional.empty();
-        boolean operationSuccess = false;
-
-        newAgent = Optional.of(agent.clone());
-        operationSuccess = AgentList.add(newAgent.get());
-
-        if (!operationSuccess) {
-            newAgent = Optional.empty();
-        }
-
-        return newAgent;
-
-    }
-
-    public Agent getAgentByEmail(String agentEmail) {
-        for (Agent agent : AgentList) {
-            if (agent.getEmail().equals(agentEmail)) {
-                return agent;
-            }
-        }
-        return null;
-    }
 
     public boolean addEmployee(Employee employee) {
         if (!employeeExists(employee.getEmail())) {
@@ -44,12 +17,21 @@ public class EmployeeRepository {
         return false;
     }
 
+    public Employee getEmployeeByEmail(String employeeEmail) {
+        for (Employee employee : employeeList) {
+            if (employee.getEmail().equals(employeeEmail)) {
+                return employee;
+            }
+        }
+        return null;
+    }
+
     public boolean employeeExists(String email) {
         return employeeList.stream().anyMatch(employee -> employee.hasEmail(email));
     }
 
-    //returns the list of agents
-    public List<Agent> getAgentList() {
-        return this.AgentList;
+    //returns the list of employees
+    public List<Employee> getEmployeeList() {
+        return this.employeeList;
     }
 }
