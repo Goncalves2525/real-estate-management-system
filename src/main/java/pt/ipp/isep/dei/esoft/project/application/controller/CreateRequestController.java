a8f1e1de6bbcfcb818985a0cc0b9186fdeeba249
cmigu <1111180@isep.ipp.pt>
2023-04-29 18:11:55 +0100

    SB - update/add comments Request controller

--- src/main/java/pt/ipp/isep/dei/esoft/project/application/controller/CreateRequestController.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/application/controller/CreateRequestController.java
@@ -8,40 +8,51 @@ import pt.ipp.isep.dei.esoft.project.repository.Repositories;
 
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
+
+/**
+ * Create Request Controller Class.
+ *
+ * This class is the controller for the user story of creating a request.
+ *
+ * This class serves as an intermediary between the user interface and the repository and domain classes.
+ */
 
 public class CreateRequestController {
-
+    /**
+     * Instantiates a new Create Request Controller.
+     *
+     * Repository instances are obtained from the Repositories class
+     */
     private AgencyListRepository agencyListRepository = null;
     private EmployeeRepository employeeRepository = null;
     private AnnouncementRepository announcementRepository = null;
     private CreateRequestController controller = null;
     private Agency[] agencyList;
+
     public CreateRequestController() {
 
-        //getAgencyListRepository();
-        //getAgentListRepository();
+
         this.employeeRepository = Repositories.getInstance().getEmployeeRepository();
         this.announcementRepository = Repositories.getInstance().getAnnouncementRepository();
         this.agencyListRepository = Repositories.getInstance().getAgencyListRepository();
     }
 
-    public List<Employee> getAgentList() {
-        return employeeRepository.getEmployeeList();
-    }
 
-    private void getAgencyListRepository() {
-
-    }
-
+    /**
+     * This method returns the list of employees from the repository
+     */
     public Employee getAgentByEmail(String agentEmail) {
         return employeeRepository.getEmployeeByEmail(agentEmail);
-
     }
 
-
-
-
+    /**
+     * This method creates an announcement for a house
+     * @param agent - agent
+     * @param transactionType - transaction type
+     * @param contractDuration - contract duration
+     * @param typeOfProperty - type of property
+     * @param property - property
+     */
     public void createannouncemntHouse(Employee agent, TransactionType transactionType, int contractDuration, TypeOfProperty typeOfProperty,Property property) {
         Date publishDate = new Date();
         Comission comission = new Comission();
@@ -49,7 +60,14 @@ public class CreateRequestController {
         Announcement announcement = new Announcement(agent, property,typeOfProperty, transactionType, publishDate, comission, photos);
         announcementRepository.addAnnouncementFromOwner(announcement);
     }
-
+    /**
+     * This method creates an announcement for an apartment
+     * @param agent - agent
+     * @param transactionType - transaction type
+     * @param contractDuration - contract duration
+     * @param typeOfProperty - type of property
+     * @param property - property
+     */
     public void createannouncemntLand(Employee agent, TransactionType transactionType, int contractDuration, TypeOfProperty typeOfProperty,Property property) {
         Date publishDate = new Date();
         Comission comission = new Comission();
@@ -57,6 +75,14 @@ public class CreateRequestController {
         Announcement announcement = new Announcement(agent, property,typeOfProperty, transactionType, publishDate, comission, photos);
         announcementRepository.addAnnouncementFromOwner(announcement);
     }
+    /**
+     * This method creates an announcement for an apartment
+     * @param agent - agent
+     * @param transactionType - transaction type
+     * @param contractDuration - contract duration
+     * @param typeOfProperty - type of property
+     * @param property - property
+     */
 
     public void createannouncemntApartment(Employee agent, TransactionType transactionType, int contractDuration, TypeOfProperty typeOfProperty,Property property) {
         Date publishDate = new Date();
@@ -66,6 +92,9 @@ public class CreateRequestController {
         announcementRepository.addAnnouncementFromOwner(announcement);
     }
 
+    /**
+     * This method returns the list of agencies
+     */
  public Agency[] AgencyOptions() {
         Agency[] agencyList = new Agency[agencyListRepository.getAgencies().size()];
         for (int i = 0; i < agencyListRepository.getAgencies().size(); i++) {
@@ -75,15 +104,6 @@ public class CreateRequestController {
         return agencyList;
     }
 
-   public Employee[] AgentOptions() {
-       Employee[] agentList = new Employee[employeeRepository.getEmployeeList().size()];
-       for (int i = 0; i < employeeRepository.getEmployeeList().size(); i++) {
-           agentList[i] = employeeRepository.getEmployeeList().get(i);
-           System.out.printf("%-8s %20s %15s %s%n", i+1, agentList[i].getName(), agentList[i].getEmail());
-
-       }
-       return agentList;
-   }
 
 
 }
