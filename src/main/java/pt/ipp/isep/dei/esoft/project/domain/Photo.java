5bca531d77bb99a08826388626612b35ab8923f1
cmigu <1111180@isep.ipp.pt>
2023-04-30 20:04:25 +0100

    SB - comments

--- src/main/java/pt/ipp/isep/dei/esoft/project/domain/Photo.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/domain/Photo.java
@@ -1,26 +1,55 @@
 package pt.ipp.isep.dei.esoft.project.domain;
 
+/**
+ * Photo class.
+ * <p>
+ * This class represents a Photo.
+ * A Photo is composed by a URI and a name.
+ */
+
 public class Photo {
     private String URI;
     private String name;
 
+    /**
+     * Instantiates a new Photo.
+     *
+     * @param URI      the uri
+     * @param fileName the file name
+     */
     public Photo(String URI, String fileName) {
         this.URI = URI;
         this.name = fileName;
     }
-
+    /**
+     * Gets uri.
+     *
+     * @return the uri
+     */
     public String getURI() {
         return URI;
     }
-
+    /**
+     * Sets uri.
+     *
+     * @param URI the uri
+     */
     public void setURI(String URI) {
         this.URI = URI;
     }
-
+    /**
+     * Gets file name.
+     *
+     * @return the file name
+     */
     public String getFileName() {
         return name;
     }
-
+    /**
+     * Sets file name.
+     *
+     * @param fileName the file name
+     */
     public void setFileName(String fileName) {
         this.name = fileName;
     }
