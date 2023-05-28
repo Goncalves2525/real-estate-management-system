596f2ceb66dc517ab3358acce2d06f128f6e98c0
Luis Leal <1100253@isep.ipp.pt>
2023-05-28 18:06:24 +0100

    SVG script run VisitScheduleRepositoryTest done

--- docs/sprintC/us009/04.tests-and-implementation/Readme.md
+++ docs/sprintC/us009/04.tests-and-implementation/Readme.md
@@ -2,7 +2,7 @@
 
 # 4. Tests 
 
-**Test 1:** Check if the given times overlap with the visit schedule - AC3.
+**Test 1:** Check if the given times overlap with the visit schedule
 
 	@Test(expected True)
     void isOverlappingTrue() {
@@ -14,7 +14,7 @@
     }
 	
 
-**Test 2:** Check if the given times overlap with the visit schedule - AC3.
+**Test 2:** Check if the given times overlap with the visit schedule
 
 	@Test (expected False)
     void isOverlappingFalse() {
@@ -25,6 +25,34 @@
         assertFalse(result, "Expected false as the given times do not overlap with the visit schedule.");
     }
 
+**Test 3:** Test if the visit schedule is added to the repository
+
+    @Test
+    void addVisitSchedule() {
+    VisitSchedule visitSchedule3 = new VisitSchedule(27,"Joaquim", 2323232324L, LocalDate.of(2023, 5, 20), LocalTime.of(11, 30), LocalTime.of(12, 30),false,"teste2@this.app");
+    repository.addVisitSchedule(visitSchedule3);
+    ArrayList<VisitSchedule> visitSchedules = repository.getVisitSchedules();
+    assertTrue(visitSchedules.contains(visitSchedule3));
+    }
+
+**Test 4:** Test if the visit schedules are returned
+
+    @Test
+    void getVisitSchedules() {
+        ArrayList<VisitSchedule> visitSchedules = repository.getVisitSchedules();
+
+        assertTrue(visitSchedules.contains(visitSchedule1));
+        assertTrue(visitSchedules.contains(visitSchedule2));
+    }
+
+**Test 5:** Test if the visit schedules are returned by the user phone number
+
+    @Test
+    void getVisitSchedulesByUserPhoneAndDate() {
+        ArrayList<VisitSchedule> visitSchedules = repository.getVisitSchedulesByUserPhoneAndDate(2323232323L, LocalDate.of(2023, 5, 28));
+
+        assertTrue(visitSchedules.contains(visitSchedule1));
+    }
 
 
 # 5. Construction (Implementation)
---
+++ src/test/java/pt/ipp/isep/dei/esoft/project/repository/VisitScheduleRepositoryTest.java
@@ -0,0 +1,52 @@
+package pt.ipp.isep.dei.esoft.project.repository;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import pt.ipp.isep.dei.esoft.project.domain.VisitSchedule;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VisitScheduleRepositoryTest {
+    private VisitScheduleRepository repository;
+    private VisitSchedule visitSchedule1;
+    private VisitSchedule visitSchedule2;
+
+    @BeforeEach
+    void setUp() {
+        repository = new VisitScheduleRepository();
+
+        visitSchedule1 = new VisitSchedule(25,"Joao", 2323232323L, LocalDate.of(2023, 5, 28), LocalTime.of(10, 30), LocalTime.of(11, 30),false,"teste@this.app");
+        visitSchedule2 = new VisitSchedule(26,"Manel", 2323232324L, LocalDate.of(2023, 5, 20), LocalTime.of(11, 30), LocalTime.of(12, 30),false,"teste2@this.app");
+
+        repository.addVisitSchedule(visitSchedule1);
+        repository.addVisitSchedule(visitSchedule2);
+    }
+
+    @Test
+    void addVisitSchedule() {
+        VisitSchedule visitSchedule3 = new VisitSchedule(27,"Joaquim", 2323232324L, LocalDate.of(2023, 5, 20), LocalTime.of(11, 30), LocalTime.of(12, 30),false,"teste2@this.app");
+        repository.addVisitSchedule(visitSchedule3);
+        ArrayList<VisitSchedule> visitSchedules = repository.getVisitSchedules();
+        assertTrue(visitSchedules.contains(visitSchedule3));
+    }
+
+    @Test
+    void getVisitSchedules() {
+        ArrayList<VisitSchedule> visitSchedules = repository.getVisitSchedules();
+
+        assertTrue(visitSchedules.contains(visitSchedule1));
+        assertTrue(visitSchedules.contains(visitSchedule2));
+    }
+
+    @Test
+    void getVisitSchedulesByUserPhoneAndDate() {
+        ArrayList<VisitSchedule> visitSchedules = repository.getVisitSchedulesByUserPhoneAndDate(2323232323L, LocalDate.of(2023, 5, 28));
+
+        assertTrue(visitSchedules.contains(visitSchedule1));
+    }
+
+}
